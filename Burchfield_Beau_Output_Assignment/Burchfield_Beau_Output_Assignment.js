2f2bb17c401cb09c73aa5adeb57f9bc07cafe904
brburchfield <beauburchfield@Beaus-MacBook-Pro.local>
2015-03-05 16:54:03 -0600

    Added output comment
    Small change. Added comment to output.

--- Burchfield_Beau_Output_Assignment/Burchfield_Beau_Output_Assignment.js
+++ Burchfield_Beau_Output_Assignment/Burchfield_Beau_Output_Assignment.js
@@ -22,6 +22,7 @@ myOccupation = "Inventor";
 siblingNumber = 5;
 goneAbroad = false;
 
+//Outputting variable values
 console.log("My name is Beau Burchfield. I am an" + myOccupation + " with a company called Global Extreme Mobility.");
 console.log("I have" + siblingNumber + "brothers and sisters.");
 console.log("It is" + goneAbroad + "that I have been somewhere out of this country.");
