5f2127b9952e83836b4c57ef744f2b5627c6f2e2
brburchfield <beauburchfield@Beaus-MacBook-Pro.local>
2015-03-05 16:52:20 -0600

    Added output
    Added string with variables to output. Also removed redundant variable
    calls to variable value assignment. Additionally, added space between
    initial variable declarations for readability.

--- Burchfield_Beau_Output_Assignment/Burchfield_Beau_Output_Assignment.js
+++ Burchfield_Beau_Output_Assignment/Burchfield_Beau_Output_Assignment.js
@@ -9,14 +9,20 @@
 
 // Assigning a text string
 var myOccupation;
+
 // Assigning a number
 var siblingNumber;
+
 // Assigning a Boolean value
 var goneAbroad;
 
 
 // Adding value to variables
-var myOccupation = "Inventor";
-var siblingNumber = 5;
-var goneAbroud = false;
+myOccupation = "Inventor";
+siblingNumber = 5;
+goneAbroad = false;
+
+console.log("My name is Beau Burchfield. I am an" + myOccupation + " with a company called Global Extreme Mobility.");
+console.log("I have" + siblingNumber + "brothers and sisters.");
+console.log("It is" + goneAbroad + "that I have been somewhere out of this country.");
 
