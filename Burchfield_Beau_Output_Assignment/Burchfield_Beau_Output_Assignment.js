b39bad663ae16cfb759d56b5c7a6eec29536e748
brburchfield <beauburchfield@Beaus-MacBook-Pro.local>
2015-03-05 16:27:34 -0600

    Added value to variables
    Added a value to each variable type. Also, added name, date and class
    comments to top of code.

--- Burchfield_Beau_Output_Assignment/Burchfield_Beau_Output_Assignment.js
+++ Burchfield_Beau_Output_Assignment/Burchfield_Beau_Output_Assignment.js
@@ -2,9 +2,21 @@
  * Created by beauburchfield on 3/5/15.
  */
 
+//Beau Burchfield
+// 3-5-2015
+//Programmable Data Infrastructure
+
+
 // Assigning a text string
 var myOccupation;
 // Assigning a number
 var siblingNumber;
 // Assigning a Boolean value
 var goneAbroad;
+
+
+// Adding value to variables
+var myOccupation = "Inventor";
+var siblingNumber = 5;
+var goneAbroud = false;
+
