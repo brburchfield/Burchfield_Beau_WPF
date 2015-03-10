0be28cad740f260fb6e4704a4a7e61278f52ec2f
brburchfield <beauburchfield@beaus-mbp.attlocal.net>
2015-03-09 20:57:22 -0500

    Added complete prompt and alert file
    Added complete prompt and alert file

---
+++ PromptAndAlert/script.js
@@ -0,0 +1,9 @@
+/**
+ * Created by beauburchfield on 3/9/15.
+ */
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width.");
+var height = prompt("Please enter the height.");
+var area = width * height; //calculates with that info
+//console.log(area); //prints it out to console
+var result = "The area of your rectangle is " + area + ".";
+alert(result);
