471fb9dce5ec16a0f0537ec0abe81a056526b16e
brburchfield <beauburchfield@beaus-mbp.attlocal.net>
2015-03-10 21:11:21 -0500

    Created original framework TODO comments for problems
    Added Problem titles and TODOs to begin the organization of the
    problems.

--- Week_2/Expressions_worksheet/JS/script.js
+++ Week_2/Expressions_worksheet/JS/script.js
@@ -4,3 +4,59 @@
  Expression Worksheet
  */
 
+
+// Dog Years
+
+//TODO: Define variables.
+
+//TODO: Create Calculation.
+
+//TODO: Create Output.
+
+
+
+
+
+// Slice of Pie, Pt. 1
+
+//TODO: Define variables.
+
+//TODO: Create Calculation.
+
+//TODO: Create Output.
+
+
+
+
+
+// Slice of Pie, Pt. 2
+
+//TODO: Define variables.
+
+//TODO: Create Calculation.
+
+//TODO: Create Output.
+
+
+
+
+
+// Average Shopping Bill
+
+//TODO: Define variables.
+
+//TODO: Create Calculation.
+
+//TODO: Create Output.
+
+
+
+
+
+// Discounts
+
+//TODO: Define variables.
+
+//TODO: Create Calculation.
+
+//TODO: Create Output.
