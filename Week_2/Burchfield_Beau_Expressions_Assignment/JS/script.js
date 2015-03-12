1e647a6ba6c63d5d811e80f50ab107b7afab2349
brburchfield <beauburchfield@beaus-mbp.attlocal.net>
2015-03-11 23:45:37 -0500

    Created comments.
    Created comments, rather than TODOs, to frame intended code.

--- Week_2/Burchfield_Beau_Expressions_Assignment/JS/script.js
+++ Week_2/Burchfield_Beau_Expressions_Assignment/JS/script.js
@@ -3,3 +3,42 @@ Beau Burchfield
 3/11/2015
 Expressions Assignment
  */
+
+//Extra Funds Calculator
+
+//Declare weekly income.
+
+//Declare monthly bills variable.
+
+//Declare yearly donations variable.
+
+//Declare extra funds variable.
+
+
+
+
+
+
+//Create original welcome string.
+
+
+//Request weekly income.
+
+//Request monthly bills.
+
+//Inquire as to whether user donates money to charity.
+
+//If yes, prompt for yearly donation amount.
+
+
+
+//Convert weekly income to yearly. Assign result to variable.
+
+//Convert monthly bills to yearly. Assign result to variable.
+
+//Calculate yearly income, minus yearly bills and donations. Assign result to variable.
+
+
+//Print total extra funds to console.
+
+//Display totalExtra to user.
