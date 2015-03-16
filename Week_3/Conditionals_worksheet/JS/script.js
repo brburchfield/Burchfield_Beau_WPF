9dba3747f44520a219be3e7b60759a2aa7f8d756
brburchfield <beauburchfield@Beaus-MacBook-Pro.local>
2015-03-16 16:06:12 -0500

    Declared Variables
    Declared initial variables. Will add output variables later.

--- Week_3/Conditionals_worksheet/JS/script.js
+++ Week_3/Conditionals_worksheet/JS/script.js
@@ -3,3 +3,17 @@ Beau Burchfield
 3/16/2015
 Conditionals Worksheet
 */
+
+
+// ~~~Last Chance for Gas!~~~
+
+
+//--Declaring Variables--
+
+//Gas efficiency of the car
+var gasEfficiency;
+//Gauge reading of the gas tank (in %)
+var gaugeReading;
+//Car's gas tank capacity (in gallons)
+var tankCapacity;
+
