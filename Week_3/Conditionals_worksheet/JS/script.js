4d9db2447a0db09cac7d42d88b566b1a0e742c92
brburchfield <beauburchfield@Beaus-MacBook-Pro.local>
2015-03-16 17:00:28 -0500

    Added calculations
    Added calculations to simplify variables

--- Week_3/Conditionals_worksheet/JS/script.js
+++ Week_3/Conditionals_worksheet/JS/script.js
@@ -16,12 +16,23 @@ var gasEfficiency;
 var gaugeReading;
 //Car's gas tank capacity (in gallons)
 var tankCapacity;
+//Car's total gas amount
+var totalGas;
+//Car's maximum mileage
+var maxMileage;
 
 //--Assigning values to variables--
 
 //Prompt for gas efficiency
 gasEfficiency = prompt("What is your vehicle's average fuel efficiency (in mpg)?");
 //Prompt for reading of gas tank
-gaugeReading = prompt("What is the current reading of your fuel guage (in %?) \nExample: 90 is 90%.");
+gaugeReading = prompt("What is the current reading of your fuel guage (in %)? \nExample: .90 = 90%.");
 //Prompt for tank capacity
 tankCapacity = prompt("What is your gas tank capacity (in gallons)?");
+
+//--Simplifying the problem (Calculating variables)---
+
+//Calculating total gas
+totalGas = tankCapacity * gaugeReading;
+//Calculating maximum mileage
+maxMileage = totalGas * gasEfficiency;
