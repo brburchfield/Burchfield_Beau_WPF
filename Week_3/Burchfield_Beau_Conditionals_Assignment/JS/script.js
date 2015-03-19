a89381a6faca13d5fa8590c5ddd65d5fd5b8253e
brburchfield <beauburchfield@Beaus-MacBook-Pro.local>
2015-03-19 14:15:11 -0500

    Created initial Conditionals Assignment files
    Created initial Conditionals Assignment files. Linked HTML to script.
    Added general TODOs.

---
+++ Week_3/Burchfield_Beau_Conditionals_Assignment/JS/script.js
@@ -0,0 +1,12 @@
+/*
+Beau Burchfield
+3/19/2015
+Conditionals Assignment
+ */
+
+
+//TODO: Declare variables (including array).
+
+//TODO: Assign values to variables (via prompts).
+
+//TODO: Create calculations.
