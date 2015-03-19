f48c450a57b33b94dfc40d706b0b57b7f7511ed9
brburchfield <beauburchfield@Beaus-MacBook-Pro.local>
2015-03-19 15:26:21 -0500

    Cast string inputs to numbers.
    Cast string inputs to numbers. Also added comments to previous work.

--- Week_3/Burchfield_Beau_Conditionals_Assignment/JS/script.js
+++ Week_3/Burchfield_Beau_Conditionals_Assignment/JS/script.js
@@ -5,7 +5,7 @@ Conditionals Assignment
  */
 
 
-//TODO: Declare variables (including array).
+//Declare variables (including array).
 var userCost;
 var guestNumber;
 var guest1Cost;
@@ -14,12 +14,16 @@ var guest3Cost;
 var guest4Cost;
 var guest5Cost;
 
-//TODO: Assign values to variables (via prompts).
+//Assign values to variables (via prompts).
 guestNumber = prompt("How many guests do you have?", "Maximum is 5.");
+//Cast string input to number.
 guestNumber = Number(guestNumber);
+
 if(guestNumber > 5){
+//For if the user types a number over the maximum amount (5).
     alert("The maximum amount of guests you may enter is 5. Please reload the page.");
 } else if(guestNumber >= 0){
+//For if the user types any of the numbers within 0-5. This is to prompt the user as many times as numbers of guests.
     if(guestNumber >= 0) {
         if (guestNumber >= 1) {
             guest1Cost = prompt("How much was your first guest's meal?", "Enter as a decimal. (i.e. 19.20 = $19.20)");
@@ -39,9 +43,19 @@ if(guestNumber > 5){
         }
     }
 }else{
-    alert("I'm sorry. I don't understand your answer. Please reload the page, and enter an answer between 1 and 5.")
+//For if the user types anything that is not 0-5.
+    alert("I'm sorry. I don't understand your answer. Please reload the page, and enter an answer between 0 and 5.")
 }
+//Ask the user his meal cost.
+userCost = prompt("Alright. How much did your meal cost?");
 
+//Cast string inputs to numbers.
+userCost   = Number(userCost);
+guest1Cost = Number(guest1Cost);
+guest2Cost = Number(guest2Cost);
+guest3Cost = Number(guest3Cost);
+guest4Cost = Number(guest4Cost);
+guest5Cost = Number(guest5Cost);
 
 
 //TODO: Create calculations.
