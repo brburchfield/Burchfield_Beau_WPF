026c9d86fc5d6c7d71f710eb184e24aa30eebb69
brburchfield <beauburchfield@beaus-mbp.attlocal.net>
2015-03-23 21:06:19 -0500

    Created initial Functions Worksheet files
    Created initial Functions Worksheet files

---
+++ Week_4/Functions_worksheet/JS/script.js
@@ -0,0 +1,38 @@
+/*
+Beau Burchfield
+3/23/2015
+Functions Worksheet
+ */
+
+//~~~Circumference~~~
+
+//TODO: Calculate the circumference of a circle.
+
+//Parameter(s) for function:
+//Radius of the circle
+
+//Return:
+//Circumference of the circle
+
+//Result to print to the console:
+//“The circumference of the circle is X”;
+
+
+
+
+//~~~Stung!~~~
+
+//It takes 8.666666667 bee stings per pound to kill an animal.
+//Calculate how many bee stings are needed to kill an animal in a function
+
+//Given:
+//Victim’s weight (in pounds)
+
+//Parameter(s) for function:
+//Victim’s weight (in pounds)
+
+//Return:
+//Number of Bee stings
+
+//Result to print to the console:
+//“It takes X bee stings to kill this animal.
