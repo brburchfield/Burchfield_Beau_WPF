f25c7a635c1d0071faeeadc2abb62525595e7b80
brburchfield <beauburchfield@beaus-mbp.attlocal.net>
2015-03-23 21:23:41 -0500

    Created "Circumference" function
    Created "Circumference" function

--- Week_4/Functions_worksheet/JS/script.js
+++ Week_4/Functions_worksheet/JS/script.js
@@ -4,15 +4,18 @@ Beau Burchfield
 Functions Worksheet
  */
 
-//~~~Circumference~~~
+//TODO ~~~Circumference~~~
 
-//TODO: Calculate the circumference of a circle.
+//Calculate the circumference of a circle.
 
 //Parameter(s) for function:
 //Radius of the circle
-
+function calcCircle(r) {
+    var circumference = (2 * r) * 3.14;
 //Return:
 //Circumference of the circle
+    return circumference;
+}
 
 //Result to print to the console:
 //“The circumference of the circle is X”;
@@ -20,7 +23,7 @@ Functions Worksheet
 
 
 
-//~~~Stung!~~~
+//TODO ~~~Stung!~~~
 
 //It takes 8.666666667 bee stings per pound to kill an animal.
 //Calculate how many bee stings are needed to kill an animal in a function
@@ -36,3 +39,12 @@ Functions Worksheet
 
 //Result to print to the console:
 //“It takes X bee stings to kill this animal.
+
+var total = calcArea(30, 20);
+
+function calcArea(w, h){
+    var area = w * h;
+    return area;
+}
+console.log(total);
+
