30b6217b6fdd23563d10b9f5be991b7ff67e7e83
brburchfield <beauburchfield@Beaus-MacBook-Pro.local>
2015-03-26 15:05:21 -0500

    Created variables
    Created variables, including prompt and validation of whichLotto
    variable.

--- Week_4/Burchfield_Beau_Functions_Assignment/JS/script.js
+++ Week_4/Burchfield_Beau_Functions_Assignment/JS/script.js
@@ -4,3 +4,21 @@
  3/25/2015
  Functions Assignment
  */
+
+//TODO: Variables
+
+//Prompt for lotto type (spacing for aesthetics).
+var whichLotto = prompt("Do you want numbers for the Powerball or the Florida lottery?\n\n " +
+"        Type PB for Powerball, and FL for Florida lottery.");
+
+//Make user input uppercase to match variable requirements
+whichLotto = whichLotto.toUpperCase();
+
+//Validation. If nothing is entered,
+while(whichLotto == null || (whichLotto !== "PB" && whichLotto !== "FL")) {
+    whichLotto = prompt("You must enter either PB or FL.");
+    whichLotto = whichLotto.toUpperCase();
+}
+
+
+console.log(whichLotto);
